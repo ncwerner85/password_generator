5cb8c550ac3beea66512956798bd6eac0413c2b8
Nicholas Werner <ncwerner@gmail.com>
2020-10-16 14:39:03 -0700

    moved prompts to go off with btn click

--- script.js
+++ script.js
@@ -14,61 +14,63 @@
 // WHEN the password is generated
 // THEN the password is either displayed in an alert or written to the page
 
-    // Assignment Code
-// Create prompts for when the button is clicked: 
-  var generateBtn = document.querySelector("#generate");
+// Assignment Code
+// Create prompts for when the button is clicked:
+var generateBtn = document.querySelector("#generate");
 
 // prompted for the length of the password at least 8 characters and no more than 128 characters
-  var userPasswordSize = prompt("Choose a password length between 8 - 128 characters");
+// prompted for character types to include in the password: lowercase, uppercase, numeric, and/or special characters
+
+var altVarCharLow = "qwertyuiopasdfghjklzxcvbnm";
+var altVarCharUp = "QWERTYUIOPASDFGHJKLZXCVBNM";
+var altVarCharSpec = "!@#$%^&*";
+var altVarCharNum = "1234567890";
+
+function generatePassword() {
+  var userPasswordSize = prompt(
+    "Choose a password length between 8 - 128 characters"
+  );
+
   var userLoCase = confirm("Would you like to use lower case letters?");
   var userUpCase = confirm("Would you like to use upper case letters?");
   var userSpecial = confirm("Would you like to use special characters?");
   var userNumber = confirm("Would you like to use numbers?");
   var hold = [];
   var randomPassword = "";
-  
-var altVarCharLow = "qwertyuiopasdfghjklzxcvbnm"
-var altVarCharUp = "QWERTYUIOPASDFGHJKLZXCVBNM"
-var altVarCharSpec = "!@#$%^&*"
-  var altVarCharNum = "1234567890"
 
-function generatePassword() {
   if (userLoCase) {
-    hold = hold + altVarCharLow
-    }if (userUpCase) {
-    hold = hold + altVarCharUp
-    }if (userSpecial) {
-      hold = hold + altVarCharSpec
-    }if (userNumber) {
-      hold = hold + altVarCharNum
-    console.log(hold)
+    hold = hold + altVarCharLow;
   }
-  for (var i = 0; i < userPasswordSize; i++){
-    randomPassword = randomPassword + hold.charAt(Math.floor(Math.random() * Math.floor(hold.length)))
+  if (userUpCase) {
+    hold = hold + altVarCharUp;
   }
-  return randomPassword
-}  
+  if (userSpecial) {
+    hold = hold + altVarCharSpec;
+  }
+  if (userNumber) {
+    hold = hold + altVarCharNum;
+    console.log(hold);
+  }
+  for (var i = 0; i < userPasswordSize; i++) {
+    randomPassword += hold.charAt(
+      Math.floor(Math.random() * Math.floor(hold.length))
+    );
+  }
+  return randomPassword;
+}
 
-// prompted for character types to include in the password: lowercase, uppercase, numeric, and/or special characters
-
-// Validate the imput
-
-    // Write password to the #password input
+// Write password to the #password input
 
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-  
-  
 
   passwordText.value = password;
-  
-//password is generated that matches the selected criteria
 
-// password is either displayed in an alert or written to the page
-
+  //password is generated that matches the selected criteria
+  // Validate the imput
+  // password is either displayed in an alert or written to the page
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
---
+++ script2.js
@@ -0,0 +1,17 @@
+  // var smallLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  // var bigLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  // var specialCharacter = ["!", "#", "$", "%", "&", "*", "+", "?", "@"];
+  // var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+
+    // if (userLoCase) {
+  //   hold.push(smallLetters)
+  //   }if (userUpCase) {
+  //   hold.push(bigLetters)
+  //   }if (userSpecial) {
+  //   hold.push(specialCharacter)
+  //   }if (userNumber) {
+  //   hold.push(numbers)
+  //   console.log(hold)
+  // }
+
+
